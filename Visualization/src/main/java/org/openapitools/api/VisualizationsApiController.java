a2288ef22ac1f96e809c20ddca914d3028ac125e
samuelgoomez <samuelgomezgrande2003@gmail.com>
2024-11-19 17:07:58 +0100

    SCRUM-30-HU22.-Ver-historial

--- Visualization/src/main/java/org/openapitools/api/VisualizationsApiController.java
+++ Visualization/src/main/java/org/openapitools/api/VisualizationsApiController.java
@@ -1,5 +1,6 @@
 package org.openapitools.api;
 
+import org.openapitools.dao.VisualizationDAO;
 import org.openapitools.model.Visualizacion;
 import org.openapitools.model.VisualizationsUserIDDelete200Response;
 
@@ -27,21 +28,48 @@ import java.util.Map;
 import java.util.Optional;
 import javax.annotation.Generated;
 
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2024-11-11T16:32:34.104885200+01:00[Europe/Madrid]", comments = "Generator version: 7.9.0")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2024-11-06T22:57:10.884527400+01:00[Europe/Madrid]", comments = "Generator version: 7.9.0")
 @Controller
 @RequestMapping("${openapi.aPIDeVisualizacionesYRecomendaciones.base-path:/views}")
 public class VisualizationsApiController implements VisualizationsApi {
 
     private final NativeWebRequest request;
+    private final VisualizationDAO visualizationDAO;
 
     @Autowired
-    public VisualizationsApiController(NativeWebRequest request) {
+    public VisualizationsApiController(NativeWebRequest request, VisualizationDAO visualizationDAO) {
         this.request = request;
+        this.visualizationDAO = visualizationDAO;
     }
 
     @Override
     public Optional<NativeWebRequest> getRequest() {
         return Optional.ofNullable(request);
+    }
+    
+    @Override
+    public ResponseEntity<List<Visualizacion>> visualizationsUserIDGet(@PathVariable("userID") Integer userID) {
+    	List<Visualizacion> list = visualizationDAO.getVisualizationsByUserId(userID);
+
+        if (!list.isEmpty()) {
+            return ResponseEntity.ok(list);  // Devuelve la película si existe
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();  // 404 si no existe
+        }
+    
+    }
+    
+    @Override
+    public ResponseEntity<VisualizationsUserIDDelete200Response> visualizationsUserIDDelete (@PathVariable("userID") Integer userID) {
+    	boolean respuesta = visualizationDAO.deleteVisualizationsByUserId(userID);
+    	VisualizationsUserIDDelete200Response mensaje = new VisualizationsUserIDDelete200Response();
+    	
+    	if (respuesta) {
+    		mensaje.message("Hitorial Borrado");
+            return ResponseEntity.ok(mensaje);  // Devuelve la película si existe
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();  // 404 si no existe
+        }
     }
 
 }
